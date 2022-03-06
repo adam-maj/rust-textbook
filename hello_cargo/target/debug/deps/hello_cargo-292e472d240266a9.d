86e962bffc957ec0afbfcb022822b4aee086aac3
adam-maj <mr.adam.maj@gmail.com>
2022-03-06 15:44:54 -0500

    Chapter 1: Getting Started

---
+++ hello_cargo/target/debug/deps/hello_cargo-292e472d240266a9.d
@@ -0,0 +1,5 @@
+/Users/adam/Desktop/rust/rust-textbook/hello_cargo/target/debug/deps/hello_cargo-292e472d240266a9: src/main.rs
+
+/Users/adam/Desktop/rust/rust-textbook/hello_cargo/target/debug/deps/hello_cargo-292e472d240266a9.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/adam/Desktop/rust/rust-textbook/hello_cargo/target/debug/hello_cargo: /Users/adam/Desktop/rust/rust-textbook/hello_cargo/src/main.rs
